86ba64dab75bf438403bdb6be613be2e247a69d4
pasqualeandriano97 <151465437+pasqualeandriano97@users.noreply.github.com>
2024-03-04 14:15:15 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ciao Mondo!");
         System.out.println("Secondo Log");
+        String stringa="Ciao questa è la prima stringa";
+        System.out.println("Prima stringa "+ stringa);
+        System.out.println("La lunghezza della prima stringa è " +stringa.length());
+        System.out.println("Il primo carattere della stringa è "+stringa.charAt(0));
+        
     }
 }
